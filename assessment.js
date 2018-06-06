0aa60e8a7c1237145f77d7153a902f99d964146b
Ryan Brockhoff <ryanbrockhoff@Ryans-MacBook-Pro.local>
2018-06-06 10:08:44 -0600

    update name,favNum var

--- assessment.js
+++ assessment.js
@@ -1,10 +1,10 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
-// CODE HERE
+var myName = "Ryan";
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
-// CODE HERE
+var faveNum = "7";
 
 // #3 create a variable called 'lovesCode' and set it to true
 
